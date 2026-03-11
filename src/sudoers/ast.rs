48e48b0145432cbe3f9042bc9a6441429ba791a0
Marc Schoolderman <marc@tweedegolf.com>
2026-03-11 10:09:25 +0100

    allow space between Defaults@ and hostname list

--- src/sudoers/ast.rs
+++ src/sudoers/ast.rs
@@ -663,13 +663,14 @@ fn get_directive<'a>(
             const DEFAULTS_LEN: usize = "Defaults".len();
             let allow_scope_modifier = stream.get_pos().0 == begin_pos.0
                 && (stream.get_pos().1 - begin_pos.1 == DEFAULTS_LEN
-                    || keyword.len() > DEFAULTS_LEN);
+                    || keyword[DEFAULTS_LEN..].starts_with('@'));
 
             let scope = if allow_scope_modifier {
                 if keyword[DEFAULTS_LEN..].starts_with('@') {
                     // Backtrack and start parsing following the '@' sign
                     *stream = begin_state;
                     stream.advance(DEFAULTS_LEN + 1);
+                    skip_trailing_whitespace(stream)?;
 
                     ConfigScope::Host(expect_nonterminal(stream)?)
                 } else if is_syntax(':', stream)? {
